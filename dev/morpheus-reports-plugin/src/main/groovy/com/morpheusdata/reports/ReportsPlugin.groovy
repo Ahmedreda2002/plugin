9f8fc82d15b8e23981ae2635efbb763a3ed0de3c
Ahmedreda2002 <150925808+Ahmedreda2002@users.noreply.github.com>
2026-02-03 10:20:13 +0100

    Add files via upload

---
+++ dev/morpheus-reports-plugin/src/main/groovy/com/morpheusdata/reports/ReportsPlugin.groovy
@@ -0,0 +1,26 @@
+package com.morpheusdata.reports
+
+import com.morpheusdata.core.Plugin
+
+class ReportsPlugin extends Plugin {
+
+	@Override
+	String getCode() {
+		return 'morpheus-reports-plugin'
+	}
+
+	@Override
+	void initialize() {
+		this.setName("Custom Reports")
+
+		CustomReportProvider customReportProvider = new CustomReportProvider(this, morpheus)
+		CustomAnalyticsProvider customAnalyticsProvider = new CustomAnalyticsProvider(this, morpheus)
+
+		this.pluginProviders.put(customReportProvider.code, customReportProvider)
+		this.pluginProviders.put(customAnalyticsProvider.code, customAnalyticsProvider)
+	}
+
+	@Override
+	void onDestroy() {
+	}
+}
